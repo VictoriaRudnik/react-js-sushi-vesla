52acc640fa25c8747898f147c0dbac5aec2360fb
Victoria Rudnik <victoria.sevryuk@gmail.com>
2020-12-22 21:04:16 +0300

    add header layout

---
+++ src/components/common/input/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export const Input = () => {
+  return(
+    <div>
+      g
+    </div>
+
+  )
+} 
---
+++ src/components/layouts/header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Logo from "../../../img/logo.JPG";
+import Language from "../../../img/Language.png";
+import User from "../../../img/User.png";
+import Call from "../../../img/Call.png";
+import "./style.css";
+import Search from "../../../img/search.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container_header">
+        <div>
+          <img className="header_logo" src={Logo} />
+        </div>
+        <div className="container_menu">
+          <p className="menu">Меню</p>
+          <p className="menu">Акции</p>
+          <p className="menu">Новости</p>
+          <p className="menu">О нас</p>
+          <p className="menu">Карьера</p>
+          <p className="menu">Отзывы</p>
+        </div>
+        <div className="header_container-search">
+          <div className="header_container-number">
+            <img className="header__img" src={Call} />
+            <p className="header__p-number">+375 (29) 321 00 00</p>
+          </div>
+          <div className="header__container-input-img">
+            <div className="header__container-input">
+              <img className="header__img-search" src={Search} />
+              <input className="header__input" placeholder="Поиск"></input>
+            </div>
+          </div>
+        </div>
+        <div className="container_login">
+          <img className="header__img" src={Language} />
+          <img className="header__img" src={User} />
+        </div>
+      </div>
+    </header>
+  );
+};
