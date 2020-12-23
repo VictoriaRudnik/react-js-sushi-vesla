69fb21f114ae94ffa5c785477c66b6ec8141b53c
Victoria Rudnik <victoria.sevryuk@gmail.com>
2020-12-23 22:49:57 +0300

    add basket layout

--- src/components/pages/mainPage/subdirectorySection/SubdirectorySection.js
+++ src/components/pages/mainPage/subdirectorySection/SubdirectorySection.js
@@ -3,6 +3,7 @@ import "./style.css";
 import ImageNigiri from "../../../../img/nigiri.png";
 import { MenuInfo } from "./menuInfo/MenuInfo";
 import ImageBaner from "../../../../img/banner.png";
+import ImageVector from "../../../../img/vector.png"
 
 const menuItem = [
   { img: ImageNigiri, name: "Нигири с тунцом опаленным", price: "2.90" },
@@ -23,7 +24,10 @@ export const SubdirectorySection = () => {
       <div className="main-page__container-subdirectory-section">
         <div className="main-page__container-filters">
           <p className="main-page__subdirectory-text">Раздел подкаталога</p>
-          <p>Фильтры</p>
+          <div className="main-page__div-subdirectory">
+            <img className="main-page__img-subdirectory-filters" src={ImageVector}/>
+            <p className="main-page__p-subdirectory-text">Фильтры</p></div>
+          
         </div>
         <div className="main-page__container-subdirectory-item">
           {menuItem.map((item, index) => (
@@ -39,7 +43,11 @@ export const SubdirectorySection = () => {
           </div>
         </div>
       </div>
-      <div className="main-page__container-basket"></div>
+      <div className="main-page__container-basket">
+        <div className="subdirectory__basket-text">
+          <p className="subdirectory__p-basket-text">Корзина</p>
+        </div>
+      </div>
     </div>
   );
 };
