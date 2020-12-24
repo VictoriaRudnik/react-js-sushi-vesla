b26fe5c2c230f08cf08fc6cb003e22f43273f164
Victoria Rudnik <victoria.sevryuk@gmail.com>
2020-12-24 12:48:43 +0300

    add refrence for call

--- src/components/layouts/header/Header.js
+++ src/components/layouts/header/Header.js
@@ -11,34 +11,45 @@ export const Header = (props) => {
   return (
     <header className="container_header">
       {/* //<div className="container_header"> */}
-        <div className="header__container-logo">
-          <img className="header_logo" src={Logo} />
-        </div>
-        <div className="container_menu">
-          <p className="menu">Меню</p>
-          <p className="menu">Акции</p>
-          <p className="menu">Новости</p>
-          <p className="menu">О нас</p>
-          <p className="menu">Карьера</p>
-          <p className="menu">Отзывы</p>
-        </div>
-        <div className="header_container-search">
-          <div className="header_container-number">
+      <div className="header__container-logo">
+        <img className="header_logo" src={Logo} />
+      </div>
+      <div className="container_menu">
+        <p className="menu">Меню</p>
+        <p className="menu">Акции</p>
+        <p className="menu">Новости</p>
+        <p className="menu">О нас</p>
+        <p className="menu">Карьера</p>
+        <p className="menu">Отзывы</p>
+      </div>
+      <div className="header_container-search">
+        <div className="header_container-number">
+          <a className="header__p-number" href="tel:+375293210000">
             <img className="header__img" src={Call} />
-            <p className="header__p-number">+375 (29) 321 00 00</p>
-          </div>
-          <div className="header__container-input-img">
-            <div className="header__container-input">
-              <img className="header__img-search" src={Search} />
-              <input className="header__input" placeholder="Поиск"></input>
-            </div>
+            
+          </a>
+          <a className="header__p-number" href="tel:+375293210000">
+            
+            <p>+375 (29) 321 00 00</p>
+          </a>
+          
+          {/* <p className="header__p-number">+375 (29) 321 00 00</p> */}
+        </div>
+        <div className="header__container-input-img">
+          <div className="header__container-input">
+            <img className="header__img-search" src={Search} />
+            <input className="header__input" placeholder="Поиск"></input>
           </div>
         </div>
-        <div className="container_login">
-          <img className="header__img" src={Language} />
-          <img className="header__img-focus" src={User} onClick={() => setIsLocationModalVisible(true)}/>
-        </div>
-     {/* </div> */}
+      </div>
+      <div className="container_login">
+        <img className="header__img" src={Language} />
+        <img
+          className="header__img-focus"
+          src={User}
+          onClick={() => setIsLocationModalVisible(true)}
+        />
+      </div>
     </header>
   );
 };
